2843bf8e184381fb8b0b5b7a8bffd3a62a6a4b32
Simone Celia <simone.celia@eng.it>
2024-08-27 00:22:30 +0200

    bu

--- bu.bat
+++ bu.bat
@@ -4,7 +4,7 @@ setlocal
 :: Variables
 set "srcDir=C:\Users\ThinkOpen\AppData\Roaming\Electronic Arts\Dark Age of Camelot\eden"
 set "toDir=C:\dev\DAoC\eden\backup\eden"
-for /f %%i in ('openssl rand -hex 16') do set "random_string=%%i"
+:: for /f %%i in ('openssl rand -hex 16') do set "random_string=%%i"
 
 :: Check if the destination folder already exists, otherwise create it
 if not exist "%toDir%" mkdir "%toDir%"
