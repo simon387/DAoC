c278b51f67dea516df93d12313bb26d3eb7d2163
Simone Celia <simone.celia@simonecelia.it>
2024-08-26 13:38:13 +0200

    backup

---
+++ bu.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+
+:: Variables
+set "srcDir=C:\Users\ThinkOpen\AppData\Roaming\Electronic Arts\Dark Age of Camelot\eden"
+set "toDir=C:\dev\DAoC\eden\backup\eden"
+for /f %%i in ('openssl rand -hex 16') do set "random_string=%%i"
+
+:: Check if the destination folder already exists, otherwise create it
+if not exist "%toDir%" mkdir "%toDir%"
+
+:: Move the directory and overwrite everything
+xcopy /y /s /e "%srcDir%" "%toDir%"
+
+echo Copy Done.
+
+endlocal
